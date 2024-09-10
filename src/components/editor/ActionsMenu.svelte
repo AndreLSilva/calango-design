235c4920e1da79a0734fc645f556e33951b548f1
André Lima e Silva <andrelimaesilvag@gmail.com>
2024-09-10 19:25:47 -0300

    refactor: Refactored actions menu to use buttons instead of list

--- src/components/editor/ActionsMenu.svelte
+++ src/components/editor/ActionsMenu.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
-  import { actions } from "$lib/editor/actions/actions";
+  import { editorScreen } from "$lib/stores/editor-stores";
   import { sanitizeCanvas } from "$lib/utils/canvas.utils";
   import { downloadFile } from "$lib/utils/misc.utils";
+  import Button from "../Button.svelte";
   import Card from "../Card.svelte";
-  import ListMenu from "../list-menu/ListMenu.svelte";
 
-  const handleSelectAction = (action: string) => {
-    switch (action) {
-      case "export":
-        {
-          const content = sanitizeCanvas(document.getElementById("canvas") as HTMLDivElement);
-          downloadFile("Canvas", content);
-        }
-        break;
-      case "reset":
-        window.dispatchEvent(new CustomEvent("canvas-reset", { bubbles: true }));
-        break;
-    }
+  const handleSettings = () => {
+    editorScreen.set("settings");
+  };
+
+  const handleExport = () => {
+    const content = sanitizeCanvas(document.getElementById("canvas") as HTMLDivElement);
+    downloadFile("Canvas", content);
+  };
+
+  const handleReset = () => {
+    window.dispatchEvent(new CustomEvent("canvas-reset", { bubbles: true }));
   };
 </script>
 
 <Card title="Actions" width={17}>
-  <ListMenu items={actions} onSelectItem={handleSelectAction} />
+  <Button onClick={handleSettings}>Settings</Button>
+  <Button onClick={handleExport}>Export</Button>
+  <Button onClick={handleReset}>Reset</Button>
 </Card>
